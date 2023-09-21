df7e15ad27c3964d9dd0dd2a9aaa20c4806c3594
krabat1 <istvan.orosz78@gmail.com>
2023-09-21 19:40:10 +0200

    creating first component as default component, styling layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//  {path: '', redirectTo: 'first', pathMatch: 'full'},
+  {path: '',  component: FirstComponent},
+//  {path: 'first', component: FirstComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
